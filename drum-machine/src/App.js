936a385df1e0ed2635fd155db272b495cad0d021
VickersCode <tyler31vickers@gmail.com>
2023-12-10 13:42:50 -0800

    style buttons

--- drum-machine/src/App.js
+++ drum-machine/src/App.js
@@ -10,15 +10,15 @@ function App() {
       <div id='drum-machine'>
         <div id='display'>
           <div id="drums">
-            <button>Q</button>
-            <button>W</button>
-            <button>E</button>
-            <button>A</button>
-            <button>S</button>
-            <button>D</button>
-            <button>Z</button>
-            <button>X</button>
-            <button>C</button>
+            <button className='drum-pad' id='q'>Q</button>
+            <button className='drum-pad' id='w'>W</button>
+            <button className='drum-pad' id='e'>E</button>
+            <button className='drum-pad' id='a'>A</button>
+            <button className='drum-pad' id='s'>S</button>
+            <button className='drum-pad' id='d'>D</button>
+            <button className='drum-pad' id='z'>Z</button>
+            <button className='drum-pad' id='x'>X</button>
+            <button className='drum-pad' id='c'>C</button>
           </div>
           <div id="control">
 
